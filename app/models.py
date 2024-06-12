24967f081434462f0b648ab9504e8ec65a0ec4a2
a-ct-seal <sitkina.alena2017@yandex.ru>
2024-06-12 14:50:08 +0300

    proper user initialization

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 
-from app import db, login, prediction_model
+from app import db, login
 from typing import Optional
 from sqlalchemy.ext.mutable import MutableList
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -17,7 +17,7 @@ class User(UserMixin, db.Model):
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     liked_tracks_ids: so.Mapped[list[int]] = (
         so.mapped_column(MutableList.as_mutable(sa.PickleType),
-                         default=prediction_model.get_random_recommendation(size=MAX_LIKED_TRACKS_SIZE // 4)))
+                         default=[]))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -28,7 +28,7 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def update_liked_tracks(self, new_liked_tracks):  # todo add uninitialized state
+    def update_liked_tracks(self, new_liked_tracks):
         self.liked_tracks_ids.extend(new_liked_tracks)
         if len(self.liked_tracks_ids) > MAX_LIKED_TRACKS_SIZE:
             self.liked_tracks_ids = self.liked_tracks_ids[len(self.liked_tracks_ids) - MAX_LIKED_TRACKS_SIZE:]
--- app/prediction_model.py
+++ app/prediction_model.py
@@ -13,12 +13,17 @@ class PredictionModel:
         self.initial_data = initial_data
 
     def get_prediction(self, list_ID, gen_size=60, recommend_size=20):  # todo fix warning
-        list_track = self.transformed_data.iloc[list_ID]
+        if len(list_ID) == 0:
+            base_list = self.get_random_recommendation()
+        else:
+            base_list = list_ID
+
+        list_track = self.transformed_data.iloc[base_list]
         neighbors = self.knn_model.kneighbors(list_track, return_distance=False, n_neighbors=gen_size)
         indexes = list(set(_flatten(list(map(list, neighbors)))))
-        chosen_indexes = random.choices(indexes, k=recommend_size)
+        chosen_indexes = random.sample(population=indexes, k=recommend_size)
         res = pd.DataFrame(self.initial_data.iloc[chosen_indexes])[['artist_name', 'track_name']].values.tolist()
-        return [(chosen_indexes[idx], res[idx][0] + ', ' + res[idx][1]) for idx in range(len(res))]
+        return [(chosen_indexes[idx], res[idx][0] + ' - ' + res[idx][1]) for idx in range(len(res))]
 
-    def get_random_recommendation(self, size):  # todo rewrite
-        return list(range(size))
+    def get_random_recommendation(self, size=100):
+        return random.sample(population=range(len(self.initial_data)), k=size)
